af7c63552020e3c39866a4f52a1aecf2d019078f
AnahiCampos19 <66236502+AnahiCampos19@users.noreply.github.com>
2022-12-16 14:35:03 -0300

    Add files via upload

--- js/main.js
+++ js/main.js
@@ -1,1 +1,6 @@
 function autor(){alert("Realizado por: Ana Campos. Desarrolladora FrontEnd y Tester QA. email: anahifcampos@gmail.com");}
+const btnSwitch= document.querySelector('#switch');
+btnSwitch.addEventListener('click', ()=> {
+    document.body.classList.toggle('dark');
+    btnSwitch.classList.toggle('active');
+});
