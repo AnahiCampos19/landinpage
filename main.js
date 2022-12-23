0d7f984cf51f99287017f9bae76d0409311df8d7
AnahiCampos19 <66236502+AnahiCampos19@users.noreply.github.com>
2022-12-23 20:08:03 -0300

    Add files via upload

---
+++ main.js
@@ -0,0 +1,26 @@
+function autor(){alert("Realizado por: Ana Campos. Desarrolladora FrontEnd y Tester QA. email: anahifcampos@gmail.com");}
+const btnDark= document.querySelector('#dark');
+btnSwitch.addEventListener('click', ()=> {
+    document.body.classList.toggle('dark');
+    btnSwitch.classList.toggle('active');
+});
+      const body = document.querySelector("body"),
+        toggle = document.querySelector(".toggle");
+
+      let getMode = localStorage.getItem("mode");
+      if (getMode && getMode === "dark") {
+        body.classList.add("dark");
+        toggle.classList.add("active");
+      }
+
+      toggle.addEventListener("click", () => {
+        body.classList.toggle("dark");
+
+        if (!body.classList.contains("dark")) {
+          return localStorage.setItem("mode", "light");
+        }
+        localStorage.setItem("mode", "dark");
+      });
+
+      toggle.addEventListener("click", () => toggle.classList.toggle("active"));
+
